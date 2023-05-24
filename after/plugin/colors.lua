3566fe586a36e89380d67bb11266d1cdfab880d0
Jannick Stuby <mail@jannick-stuby.com>
2023-05-24 14:11:29 +0200

    initial commit, copied mainly from theprimeagen

---
+++ after/plugin/colors.lua
@@ -0,0 +1,17 @@
+require('nightfox').setup({
+  options = {
+      transparent = true
+  }
+})
+vim.cmd("colorscheme nordfox")
+
+function ColorMyPencils(color)
+	color = color or "nordfox"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+end
+
+ColorMyPencils()
---
+++ lua/jast/init.lua
@@ -0,0 +1,33 @@
+require("jast.set")
+require("jast.remap")
+
+local augroup = vim.api.nvim_create_augroup
+local jastGroup = augroup('jast', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufWritePre"}, {
+    group = jastGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
